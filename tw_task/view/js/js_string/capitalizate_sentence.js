d94c94852dc8b93aa8a07843e431d93c99129ff4
DoloresDai <841856753@qq.com>
2018-11-28 16:59:02 +0800

    Modified array name from word to words

--- tw_task/view/js/js_string/capitalizate_sentence.js
+++ tw_task/view/js/js_string/capitalizate_sentence.js
@@ -1,10 +1,10 @@
 var sentence = "good afternoon, mr mike.";
 
 function getLetterOfEachWordCapilizated() {
-  let word = sentence.split(" ");
-  for (let i = 0; i < word.length; i++) {
-    word[i] = word[i].slice(0, 1).toUpperCase() + word[i].slice(1);
+  let words = sentence.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i].slice(0, 1).toUpperCase() + words[i].slice(1);
   }
-  return word.join(" ");
+  return words.join(" ");
 }
 document.write(getLetterOfEachWordCapilizated(sentence));
