fa85f127e90e9e96d5d8ceb1ccac330a808fa33d
DoloresDai <841856753@qq.com>
2018-12-05 07:35:31 +0800

    Packaged function.

--- tw_task/view/js/array/double_array_for.js
+++ tw_task/view/js/array/double_array_for.js
@@ -1,6 +1,8 @@
 var a = [1, 2, 3, 4, 5];
-for (i = 0; i <= a.length - 1; i++)
- {
-  a[i] = 2 * a[i];
+console.log(doubleArray(a));
+
+function doubleArray() {
+  for (i = 0; i <= a.length - 1; i++) {
+    a[i] = 2 * a[i];
+  }
 }
-console.log(a);
