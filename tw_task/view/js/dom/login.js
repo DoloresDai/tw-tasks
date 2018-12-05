76e4d88f057f89021b6f97d2d45b87db1cf5619b
DoloresDai <841856753@qq.com>
2018-12-05 10:37:06 +0800

    Modified data content

--- tw_task/view/js/dom/login.js
+++ tw_task/view/js/dom/login.js
@@ -1,13 +1,13 @@
-function isLogicSuccess() {
+function isLoginSuccess(username,password) {
   return username === "123" && password === "123";
 }
 
-function showLogicResult() {
+function showLoginResult() {
   let username = document.getElementById("username").value;
   let password = document.getElementById("password").value;
-  if (isLogicSuccess(username, password) alert("登录成功！");
+  if (isLogicSuccess(username, password)){
+      alert("登录成功！");
   }
-  else {
-    alert("账号或密码错误！");
-    password.outerHTML = password.outerHTML;
-  }
+  alert("账号或密码错误！");
+  password.outerHTML = password.outerHTML;
+}
