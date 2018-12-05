150ab1d98163f210caad4b337623fabe785a59d9
DoloresDai <841856753@qq.com>
2018-12-05 08:35:46 +0800

    Add five methods of object

---
+++ tw_task/view/js/object/revise_attribute.js
@@ -0,0 +1,14 @@
+var user = new Object();
+console.log(user);
+
+user.name = "John";
+console.log(user.name);
+
+user["surname"] = "Mike";
+console.log(user.surname);
+
+user.name = "Peter";
+console.log(user.name);
+
+delete user.name;
+console.log(user);
