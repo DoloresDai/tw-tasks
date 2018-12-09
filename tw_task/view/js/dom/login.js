a4cc39795f6db35640ff1e0939955fe6f120fede
DoloresDai <841856753@qq.com>
2018-12-10 04:28:28 +0800

    Refactore login successfully

--- tw_task/view/js/dom/login.js
+++ tw_task/view/js/dom/login.js
@@ -1,13 +1,26 @@
-function isLoginSuccess(username,password) {
-  return username === "123" && password === "123";
+function isLoginSuccess(username, password) {
+    return username === "123" && password === "123";
 }
 
 function showLoginResult() {
-  let username = document.getElementById("username").value;
-  let password = document.getElementById("password").value;
-  if (isLogicSuccess(username, password)){
-      alert("登录成功！");
-  }
-  alert("账号或密码错误！");
-  password.outerHTML = password.outerHTML;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    if (isLoginSuccess(username, password)) {
+        getFeedback("登录成功！");
+    } else {
+        getFeedback("用户或密码错误！");
+        cleanPassword();
+    }
+}
+
+function getFeedback(feedbackString) {
+    let feedback = document.createElement("p");
+    feedback.id = "feedback";
+    let result = document.createTextNode(feedbackString);
+    feedback.appendChild(result);
+    document.body.appendChild(feedback);
+}
+
+function cleanPassword() {
+    document.getElementById("password").innerHTML = "";
 }
