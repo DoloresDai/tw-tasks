5969d0ca95df0d0000ce80c724e9898c4cc70634
DoloresDai <841856753@qq.com>
2018-12-10 04:32:49 +0800

    Increase local variables in order to improve program responsiveness

--- tw_task/view/js/event/listener.js
+++ tw_task/view/js/event/listener.js
@@ -2,10 +2,12 @@ var ul = document.getElementById("no");
 var lis = ul.getElementsByTagName("li");
 
 function listener(lis) {
-  for (var i = 0; i < lis.length; i++) {
-    lis[i].onclick = function() {
-      alert(this.innerHTML);
+    let len = lis.length;
+    let i;
+    for (i = 0; i < len; i++) {
+        lis[i].onclick = function() {
+            alert(this.innerHTML);
+        }
     }
-  }
 }
 console.log(listener(lis));
